28e41ce3051496146339dd7db2df35dc66d0f21b
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-07-06 13:49:32 +0100

    Update sessionFactory.ts

--- controller-api/src/sessionFactory.ts
+++ controller-api/src/sessionFactory.ts
@@ -2,7 +2,7 @@ import fs from 'fs/promises';
 import mustache from 'mustache';
 import { k8sApi } from './k8sClient';
 
-export async function createSession(sessionId: string, os: string, resolution = '1280x720') {
+export async function createSession(sessionId: string, os: string, resolution = '1280x720', memoryLimits = '4Gi', memoryRequests = '2Gi', cpuLimits = '2', cpuRequests = '1') {
   const path = `../${os}.yaml`;
   const rawTemplate = await fs.readFile(path, 'utf-8');
 
