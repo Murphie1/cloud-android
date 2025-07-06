99ec3e1b5b2d22e4cea8c2b5c07bb252c1dfa6cb
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-07-06 13:26:03 +0100

    Update sessionFactory.ts

--- controller-api/src/sessionFactory.ts
+++ controller-api/src/sessionFactory.ts
@@ -3,15 +3,19 @@ import mustache from 'mustache';
 import { k8sApi } from './k8sClient';
 
 export async function createSession(sessionId: string, os: string, resolution = '1280x720') {
-  const path = `./os-templates/${os}.yaml`;
+  const path = `../${os}.yaml`;
   const rawTemplate = await fs.readFile(path, 'utf-8');
 
   const yaml = mustache.render(rawTemplate, {
     sessionId,
-    resolution
+    resolution,
+    memoryLimits,
+    memoryRequests,
+    cpuLimits,
+    cpuRequests,
   });
 
-  await k8sApi.createNamespacedPod({
+  await k8sApi.createNamespacedDeployment({
     namespace: 'default', 
     body: yamlToPodObject(yaml)
   });
